caf7d915986247541f899d8e208f2e2555b176ee
yongha6 <loha31707@gmail.com>
2022-05-25 10:44:26 +0900

    what?

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+public enum EStageType
+{
+    One, Two, Three, Four, Five, Six, End
+}
 public class GameManager : MonoBehaviour
 {
-    static GameManager Instance;
+    public static GameManager Instance;
+
+    public EStageType eStageType;
+    [SerializeField]
+    private RectTransform BoxParent;
+
+    [SerializeField]
+    private List<Button> Boxs;
 
     void Awake()
     {
@@ -13,16 +25,33 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
-        
+        randomInstBox();
     }
 
     void Update()
     {
-        
+
     }
 
     public void StartStageOne()
     {
-        
+        randomInstBox();
+    }
+
+    void randomInstBox()
+    {
+        var buttons = new List<Button>();
+
+        foreach (var button in Boxs)
+            buttons.Add(button);
+
+        int count = buttons.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            int num = Random.Range(0, buttons.Count);
+            Button button = Instantiate(buttons[num], BoxParent);
+            buttons.RemoveAt(num);
+        }
     }
 }

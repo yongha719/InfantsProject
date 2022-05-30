78a6979d14a6a031031bf9d5f79e6e567b2edfce
yongha6 <loha31707@gmail.com>
2022-05-30 20:45:13 +0900

    ewe

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public enum EStageState
 {
     One = 0, Two, Three, Four, Five, Six, End
 }
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public static EStageState Estagestate;
 
     [SerializeField]
     private RectTransform ParentRt;
+    private List<Transform> slots = new List<Transform>();
 
-    [SerializeField]
-    private List<List<Button>> Ingredients = new List<List<Button>>();
 
     [SerializeField]
     private List<Button> Boxs;
@@ -26,12 +26,14 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+
     }
     void Start()
     {
-        Ingredients.Add(Boxs);
-        Ingredients.Add(Bananas);
         SelectStage();
+
+        for (int i = 0; i < ParentRt.childCount; i++)
+            slots.Add(ParentRt.GetChild(i));
     }
 
     void Update()
@@ -46,12 +48,10 @@ public class GameManager : MonoBehaviour
         switch (Estagestate)
         {
             case EStageState.One:
-                RandomInstantiateButton(Ingredients[num - 1], num);
-                Invoke($"StartStage{num}");
+                RandomInstantiateButton(Boxs, num);
                 break;
             case EStageState.Two:
-                RandomInstantiateButton(Ingredients[num - 1], num);
-                //Invoke($"StartStage{num}");
+                RandomInstantiateButton(Bananas, num);
                 break;
             case EStageState.Three:
                 break;
@@ -65,26 +65,27 @@ public class GameManager : MonoBehaviour
                 Debug.Assert(false);
                 break;
         }
+        Invoke($"StartStage{num}");
     }
     public void StartStage1()
     {
-        
+
     }
 
 
     void RandomInstantiateButton(List<Button> buttons, int selectnum)
     {
-        var instbuttons = new List<Button>();
+        var slottr = new List<Transform>();
 
-        foreach (var button in buttons)
-            instbuttons.Add(button);
+        foreach (var slot in slots)
+            slottr.Add(slot);
 
-        int count = instbuttons.Count;
+        int count = slots.Count;
 
         for (int i = 0; i < count; i++)
         {
-            int num = Random.Range(0, instbuttons.Count);
-            Button button = Instantiate(instbuttons[num], ParentRt);
+            int num = Random.Range(0, slots.Count);
+            Button button = Instantiate(buttons[i], slottr[num]);
 
             button.onClick.AddListener(() =>
             {
@@ -92,15 +93,31 @@ public class GameManager : MonoBehaviour
                     CheckBasket(button.gameObject);
             });
 
-            instbuttons.RemoveAt(num);
+            slottr.RemoveAt(num);
         }
     }
     void CheckBasket(GameObject button)
     {
 
     }
+
     public void Invoke(string name)
     {
         Invoke(name, 0);
     }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
 }

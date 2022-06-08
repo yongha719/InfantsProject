cbc8f595da7f6856a796088c88d716b3e97f0fc4
yongha6 <loha31707@gmail.com>
2022-06-08 20:44:30 +0900

    익

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -6,8 +6,12 @@ public class Fade : MonoBehaviour
 {
     public static Fade Instance { get; private set; } = null;
 
-    List<RectTransform> Clouds = new List<RectTransform>();
+    RectTransform[] Clouds;
 
+    private void Awake()//Awake¾îÂ¿Æ¼ºñ
+    {
+        Clouds = Resources.LoadAll<RectTransform>("Cloud/");
+    }
     public void FadeIn()
     {
         StartCoroutine(EFadeIn());

8d33ee57ad47eaac6e0a1da3248cf0715fbaa209
yongha6 <loha31707@gmail.com>
2022-06-08 20:07:23 +0900

    SOUND ADD

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class Fade : MonoBehaviour
 {
-    
-    
+    public static Fade Instance { get; private set; } = null;
 
+    List<RectTransform> Clouds = new List<RectTransform>();
+
+    public void FadeIn()
+    {
+        StartCoroutine(EFadeIn());
+    }
+
+    IEnumerator EFadeIn()
+    {
+        yield return null;
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(EFadeOut());
+    }
+
+    IEnumerator EFadeOut()
+    {
+        yield return null;
+    }
 }
